f954ae5e95d9357f684e21e6f741534eea5c11e3
amore <807718119@qq.com>
2021-05-05 08:06:54 -0700

    organize files

---
+++ helper_test/login.js
@@ -0,0 +1,20 @@
+import pkg from 'selenium-webdriver';
+const {Builder, By, Key, until} = pkg;
+import 'chromedriver';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const account = process.env.ACCOUNT;
+const password = process.env.PASSWORD;
+
+export const initialization = async(driver) => {
+    // Open the sandbox
+    await (await driver).manage().setTimeouts({ implicit:10000 });
+    await driver.get('https://tibcocpq--sandbox.lightning.force.com/lightning/page/home');
+
+    // Log in with email and password
+    await driver.findElement(By.css('#email')).sendKeys(account);
+    await driver.findElement(By.css('#next')).click();
+    await driver.wait(until.elementLocated(By.css('#password'))).sendKeys(password);
+    await driver.wait(until.elementLocated(By.css('#taLogin'))).click();
+}
